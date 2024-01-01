8993c4b9acb63ca389d2152e1fac3ff387c7257e
yusra-habib <95533207+yusra-habib@users.noreply.github.com>
2024-01-01 11:16:56 +0500

    CreateTable-Queries.sql

---
+++ Table-Queries.sql
@@ -0,0 +1,56 @@
+--customer
+create table XX_GTM_CUSTOMER_MTO
+(
+  cnic          NUMBER primary key not null,
+  customer_name VARCHAR2(100) not null,
+  account_type  VARCHAR2(20),
+  email         VARCHAR2(100),
+  phone_number  VARCHAR2(20) not null,
+  city          VARCHAR2(50) not null,
+  area          VARCHAR2(50) not null,
+  country       VARCHAR2(50) not null
+);
+
+
+--ORDER
+create table XX_GTM_ORDER_MTO
+(
+  order_id     NUMBER primary key not null,
+  product_id   NUMBER,
+  order_date   DATE not null,
+  total_amount NUMBER not null,
+  order_status VARCHAR2(50) not null,
+  cnic         NUMBER
+);
+alter table XX_GTM_ORDER_MTO
+  add constraint FK_CNIC foreign key (CNIC)
+  references XX_GTM_CUSTOMER_MTO (CNIC);
+alter table XX_GTM_ORDER_MTO
+  add foreign key (PRODUCT_ID)
+  references XX_GTM_PRODUCT_MTO (PRODUCT_ID);
+
+--shipping
+create table XX_GTM_SHIPPING_MTO
+(
+  shipping_id            NUMBER primary key not null,
+  order_id               NUMBER,
+  shipping_date          DATE,
+  carrier_name           VARCHAR2(100),
+  tracking_number        VARCHAR2(50),
+  estimated_arrival_date DATE,
+  shipping_status        VARCHAR2(50)
+);
+alter table XX_GTM_SHIPPING_MTO
+  add foreign key (ORDER_ID)
+  references XX_GTM_ORDER_MTO (ORDER_ID);
+
+--product
+create table XX_GTM_PRODUCT_MTO
+(
+  product_id         NUMBER primary key not null,
+  product_name       VARCHAR2(100) not null,
+  category           VARCHAR2(30) not null,
+  descriptions       VARCHAR2(255),
+  price              NUMBER not null,
+  quantity_available NUMBER not null
+);
